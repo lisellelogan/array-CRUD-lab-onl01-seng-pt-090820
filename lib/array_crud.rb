d9b1574c0821529676dfd7fa879346fa0c48fcf1
Liselle Logan <liselle.logan@gmail.com>
2020-09-13 23:36:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,9 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-
+  array = ["I", "Really", "Love", "Learning", "About"]
+  element = "arrays!"
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
